2306a3dae9c5526fecaf081a3152ac60fe722cf9
Andras Kucsma <andras.kucsma@prezi.com>
2015-10-31 19:00:06 +0100

    Add argument parsing for shunting_yard test script

--- shunting_yard/test.py
+++ shunting_yard/test.py
@@ -1,32 +1,54 @@
 #!/usr/bin/env python
 
 import sys
+import argparse
 
-if len(sys.argv) != 3:
-    print "Usage: " + sys.argv[0] + " containersX.txt solutionX.txt"
+def test(containersFile, solutionFile):
+    with open(containersFile) as containers:
+        content = containers.read().splitlines()
+        original = list(content[0])
+        expected = list(content[1])
+
+    with open(solutionFile) as solution:
+        content = solution.read().splitlines()
+        count = int(content[0])
+        if count != len(content) - 1:
+            print 'Count doesn\'t match the number of lines'
+            return False
+
+        reverses = content[1:]
+
+    for reverse in reverses:
+        start, end = tuple(map(int, reverse.split()))
+        original[start:end+1] = original[start:end+1][::-1]
+
+    if original != expected:
+        print 'Expected : ', expected
+        print 'Got :      ', original
+        return False
+
+    print 'OK'
+    return True
+
+parser = argparse.ArgumentParser(description = 'Shunting yard')
+
+parser.add_argument('-c', '--containers', dest='containers',
+    help='Containers txt', required=True)
+parser.add_argument('-t', '--test', dest='test',
+    help='Solution txt (to check solution)')
+parser.add_argument('-s', '--solve', action='store_true',
+    help='Solve solution')
+
+args = parser.parse_args()
+
+print args
+if args.test is None and not args.solve:
+    print 'Specify either --solve or --test'
     sys.exit(1)
 
-with open(sys.argv[1]) as containers:
-    content = containers.read().splitlines()
-    original = list(content[0])
-    expected = list(content[1])
-
-with open(sys.argv[2]) as solution:
-    content = solution.read().splitlines()
-    count = int(content[0])
-    if count != len(content) - 1:
-        print "Count doesn't match the number of lines"
-        sys.exit(1)
-
-    reverses = content[1:]
-
-for reverse in reverses:
-    start, end = tuple(map(int, reverse.split()))
-    original[start:end+1] = original[start:end+1][::-1]
-
-if original != expected:
-    print "Expected : ", expected
-    print "Got :      ", original
+if args.test is not None and args.solve:
+    print 'Specify only one of --solve and --test'
     sys.exit(1)
 
-print "OK"
+if args.test is not None:
+    sys.exit(0 if test(args.containers, args.test) else 1)
