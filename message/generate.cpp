b3bd5602c4377c492056f7a872dd0a701ca7bbff
Andras Kucsma <andras.kucsma@prezi.com>
2015-11-10 21:19:05 +0100

    Add generator

--- message/generate.cpp
+++ message/generate.cpp
@@ -53,6 +53,43 @@ std::string textToDns(const std::string& text) {
     return ss.str();
 }
 
+void generate_decoder(const std::string& dns) {
+    auto text = dnsToText(dns);
+
+    // no attempt was made to make it shorter, yet
+    std::cout << R"RAW(
+#include <iostream>
+#include <string>
+#include <sstream>
+char bitsToDnsChar(char ch) {
+    switch (ch) {
+        case 0: return 'A';
+        case 1: return 'C';
+        case 2: return 'T';
+        case 3: return 'G';
+    }
+}
+std::string charToDnsSequence(char ch) {
+    std::string s(4, '\0');
+    for (unsigned j = 0; j < 4; ++j) {
+        s[j] = bitsToDnsChar((ch >> 2*j) & 0x03);
+    }
+    return s;
+}
+std::string textToDns(const std::string& text) {
+    std::stringstream ss;
+    for (char ch : text) {
+        ss << charToDnsSequence(ch);
+    }
+    return ss.str();
+}
+int main() {
+    auto d=R"()RAW" << text << R"RAW()";
+    std::cout << textToDns(d);
+}
+)RAW";
+}
+
 int main() {
     std::string dns;
     std::getline(std::cin, dns);
@@ -60,4 +97,6 @@ int main() {
     auto text = dnsToText(dns);
     auto dns_back = textToDns(text);
     assert(dns == dns_back);
+
+    generate_decoder(dns);
 }
