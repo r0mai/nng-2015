9b87995b2730b02a7bd4955173361ac11ce39a46
Andras Kucsma <andras.kucsma@prezi.com>
2015-11-14 18:15:08 +0100

    Remove braces (8588)

--- message/generate.cpp
+++ message/generate.cpp
@@ -285,12 +285,10 @@ char global[2];
 int main() {
     for (unsigned char ch : R"()RAW" << replaced_result.compressed_string << R"RAW()") {
         auto ptr = &(*global = ch);
-        if (ch & 128) {
+        if (ch & 128)
             ptr = m[ch - 128];
-        }
-        for (int i = 0; i < strlen(ptr)*4; ++i) {
+        for (int i = 0; i < strlen(ptr)*4; ++i)
             putchar("ACTG"[ptr[i/4] >> i%4*2 & 3]);
-        }
     }
 }
 )RAW";
